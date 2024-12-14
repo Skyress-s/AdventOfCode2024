121ee5b3270ba0969d6b5cc12d54448467402ebb
Mathias Mohn Mørch <mathmoe64@gmail.com>
2024-12-14 11:52:55 +0100

    feat: Finished Day05 Part1!

--- Days/Day05/Day05.cpp
+++ Days/Day05/Day05.cpp
@@ -7,7 +7,7 @@
 #include "Day05.h"
 
 #include <ranges>
-#include <bits/ranges_algo.h>
+#include <algorithm>
 
 #include "Base/DayEnum/DayEnum.h"
 #include "Utility/ParseUtility/ParseUtility.h"
@@ -15,6 +15,11 @@
 using PageIdType = uint16_t;
 using UpdateVectorType = std::vector<PageIdType>;
 using PageOrderingRulePairType = std::pair<PageIdType, PageIdType>;
+
+std::ostream& operator<<(std::ostream& os, const PageOrderingRulePairType& rule){
+    os << "{" << rule.first << ", " << rule.second << "}";
+    return os;
+}
 
 void PrintRules(const std::vector<PageOrderingRulePairType>& Rules){
     for (const PageOrderingRulePairType& Rule : Rules){
@@ -62,6 +67,7 @@ bool UpdatesContainsPageId(const UpdateVectorType& Updates, const PageIdType& Ru
     });
 }
 
+
 bool UpdateContainsPageOrderRule(const UpdateVectorType& Updates, const PageOrderingRulePairType& Rule)
 {
      auto filter_view = std::ranges::views::filter(Updates, [Rule](const PageIdType& PageId) {
@@ -69,6 +75,30 @@ bool UpdateContainsPageOrderRule(const UpdateVectorType& Updates, const PageOrde
     });
     return std::ranges::distance(filter_view.begin(), filter_view.end()) == 2;
 }
+
+bool DoesUpdateSatisfyRules(const UpdateVectorType& Update, const std::vector<PageOrderingRulePairType>& Rules)
+{
+    for (const PageOrderingRulePairType& Rule : Rules) {
+        // If updates does not contain the rule, we assume it to be valid
+        if (!UpdateContainsPageOrderRule(Update, Rule)) {
+            continue;
+        }
+
+        // The rule exists. Is it satisfied?
+
+        auto first_itr = std::find(Update.begin(), Update.end(), Rule.first);
+        auto second_itr = std::find(Update.begin(), Update.end(), Rule.second);
+
+        const uint16_t FirstIndex = std::distance(Update.begin(), first_itr);
+        const uint16_t SecondIndex = std::distance(Update.begin(), second_itr);
+
+        if (FirstIndex > SecondIndex) {
+            return false;
+        }
+    }
+    return true;
+}
+
 
 namespace KT::Days {
         EDay Day05::GetDay() const {
@@ -83,7 +113,17 @@ namespace KT::Days {
             PrintRules(Rules);
             PrintUpdates(Updates);
 
-            return 0;
+            uint16_t Sum = 0;
+            for (auto UpdateItr = Updates.begin(); UpdateItr != Updates.end(); UpdateItr++) {
+                const UpdateVectorType& Update = *UpdateItr;
+                const bool bSatifiesRules = DoesUpdateSatisfyRules(Update, Rules);
+                std::cout << "Update: " << std::ranges::distance(Updates.begin(), UpdateItr) << "Satisfies Rules: " << bSatifiesRules << "\n";
+                if (bSatifiesRules){
+                    Sum += Update[Update.size() / 2];
+                }
+            }
+
+            return Sum;
         }
 
         int32_t Day05::SolvePart2(const std::vector<StringType> &Input) {
