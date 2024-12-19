a5351fafcbda1e96fd9da77806ebf07459a86f85
Mathias M <mathmoe64@gmail.com>
2024-12-19 08:50:36 +0100

    feat: Working on day 5.
    Also learning views!

--- Days/Day06/Day06.cpp
+++ Days/Day06/Day06.cpp
@@ -11,6 +11,7 @@
 #include <unordered_set>
 #include <iostream>
 #include <ranges>
+#include <set>
 
 #include "Base/DayEnum/DayEnum.h"
 #include "Base/Vector2D/Vector2D.h"
@@ -49,7 +50,7 @@ public:
 		return Vector.X < 0 || Vector.Y < 0 || Vector.X >= m_Grid[0].size() || Vector.Y >= m_Grid.size();
 	}
 
-	void PrintWithPath(const std::unordered_set<Math::SVector2I>& VisitedLocations)
+	void PrintWithPath(const std::unordered_set<Math::SVector2I>& VisitedLocations) const
 	{
 		for (std::vector<std::vector<bool>>::const_reverse_iterator YItr = m_Grid.crbegin(); YItr != m_Grid.crend(); ++YItr)
 		{
@@ -70,6 +71,8 @@ public:
 		}
 	}
 
+	const std::vector<std::vector<bool>>& GetGridConst() const { return m_Grid; }
+
 private:
 	std::vector<std::vector<bool>> m_Grid;
 	Math::SVector2I m_GuardLocation{};
@@ -81,7 +84,7 @@ private:
 			const auto Row = *YItr;
 			for (auto XItr = Row.begin(); XItr != Row.end(); ++XItr)
 			{
-				const uint8_t YIndex = std::ranges::distance(YItr, Grid.m_Grid.rend()) - 1;
+				const uint8_t YIndex = std::ranges::distance(YItr, Grid.m_Grid.crend()) - 1;
 				const uint8_t XIndex = std::ranges::distance(Row.begin(), XItr);
 				if (Math::SVector2I(XIndex, YIndex) == Grid.m_GuardLocation)
 				{
@@ -161,6 +164,22 @@ Math::SVector2I ProcessGuard(SGrid& Grid, const Math::SVector2I& Direction)
 	return Direction;
 }
 
+// void PrintGridWithPath(const SGrid& Grid, const std::unordered_set<Math::SVector2I>& VisitedLocations)
+// {
+// 	for (auto itr = Grid.GetGridConst().crbegin(); itr != Grid.GetGridConst().crend(); ++itr)
+// 	{
+// 		const uint16_t YIndex = std::distance(Grid.GetGridConst().crend() + 1, itr);
+// 		const std::vector<bool>& Row = *itr;
+// 		for (auto XItr = Row.cbegin(); XItr != Row.cend(); ++XItr)
+// 		{
+// 			const bool Value = *XItr;
+// 			if ()
+// 		}
+//
+// 	}
+// }
+
+
 namespace KT::Days
 {
 EDay Day06::GetDay() const
@@ -200,53 +219,127 @@ int32_t Day06::SolvePart1(const std::vector<StringType>& Input)
 	return (int32_t)VisitedLocations.size();
 }
 
+std::ostream& operator<<(std::ostream& os, const std::set<Math::SVector2I>& Set)
+{
+	os << "Set{";
+	for (auto itr = Set.begin(); itr != Set.end(); ++itr)
+	{
+		os << *itr << '\n';
+	}
+	return os << "}\n";
+}
+
+enum EDirection
+{
+	Up, Down, Left, Right
+};
+
+EDirection GetDirection(const Math::SVector2I& From, const Math::SVector2I& To)
+{
+	if (From.X < To.X)
+	{
+		return Right;
+	}
+	if (From.X > To.X)
+	{
+		return Left;
+	}
+	if (From.Y < To.Y)
+	{
+		return Up;
+	}
+	if (From.Y > To.Y)
+	{
+		return Down;
+	}
+}
+
+bool ProcessPath(const SGrid& Grid, const Math::SVector2I& Location, const Math::SVector2I& Direction, const Math::SVector2I& NewObstruction)
+{
+	std::vector<std::pair<Math::SVector2I, EDirection>> VisitedLocations;
+	Math::SVector2I CurrentLocation = Location;
+	Math::SVector2I CurrentDirection = Direction;
+
+	uint8_t NumMatches{0};
+	bool bHasLooped{false};
+
+	VisitedLocations.emplace_back(CurrentLocation, EDirection::Up);
+
+	while (true)
+	{
+		Math::SVector2I NewPosition = CurrentLocation + CurrentDirection;
+
+		if (Grid.IsLocationOOB(NewPosition))
+		{
+			return false;
+		}
+
+		Math::SVector2I NewDirection = CurrentDirection;
+		if (IsPositionBlocked(Grid, NewPosition) || NewPosition == NewObstruction)
+		{
+			CurrentDirection = RotateDirectionClockwise(CurrentDirection);
+		}
+		else
+		{
+			std::pair<Math::SVector2I, EDirection> New {NewPosition, GetDirection(CurrentLocation, NewDirection)};
+
+			std::ranges::all_of(VisitedLocations, [New](const auto& VisitedLocation)
+			{
+				return VisitedLocation == New.first;
+			});
+
+			VisitedLocations.emplace_back();
+			CurrentLocation = NewPosition;
+
+		}
+	}
+}
+
 int32_t Day06::SolvePart2(const std::vector<StringType>& Input)
 {
-	std::vector<Math::SVector2I> VisitedLocations{};
+	// std::vector<Math::SVector2I> VisitedLocations{};
 	// std::unordered_set<Math::SVector2I> VisitedLocations{};
 	uint16_t PossibleObstructions = 0;
 	SGrid Grid = BuildGridAndGetStartLocation(Input);
 	// std::cout << Grid << '\n'; // that's so cool!
 	Math::SVector2I Direction{0, 1};
 
-	std::cout << Grid.GetGuardLocation() << '\n';
-	VisitedLocations.emplace_back(Grid.GetGuardLocation());
+	uint16_t NumMatches{0};
 
+	std::unordered_set<Math::SVector2I> Obstructions{};
+
+	std::unordered_set<Math::SVector2I> VisitedLocations;
+	Math::SVector2I CurrentLocation = Grid.GetGuardLocation();
+	Math::SVector2I CurrentDirection = Direction;
+	VisitedLocations.emplace(CurrentLocation);
 	while (true)
 	{
-		// std::cout << Grid << '\n'; // that's so cool!
-		Direction = ProcessGuard(Grid, Direction);
-		if (Grid.IsLocationOOB(Grid.GetGuardLocation()))
+		Math::SVector2I NewPosition = CurrentLocation + CurrentDirection;
+
+		if (Grid.IsLocationOOB(NewPosition))
 		{
 			break;
 		}
 
-		if (Grid.GetGuardLocation() == VisitedLocations.back()) // Not same as last element
-			continue;
-
-		// std::cout << Grid.GetGuardLocation() << '\n';
-		auto FoundElement = std::find(VisitedLocations.begin(), VisitedLocations.end(), Grid.GetGuardLocation());
-		// auto FoundElement = VisitedLocations.find(Grid.GetGuardLocation());
-
-		// if (FoundElement != VisitedLocations.end()) // found element
-		// {
-			const Math::SVector2I NextLocation = Grid.GetGuardLocation() + RotateDirectionClockwise(Direction);
-
-			if (*(FoundElement+1) == NextLocation)
+		Math::SVector2I NewDirection = CurrentDirection;
+		if (IsPositionBlocked(Grid, NewPosition))
+		{
+			CurrentDirection = RotateDirectionClockwise(CurrentDirection);
+		}
+		else
+		{
+			CurrentLocation = NewPosition;
+			if (ProcessPath(Grid, NewPosition, RotateDirectionClockwise(CurrentDirection), NewPosition + CurrentDirection))
 			{
-                    PossibleObstructions++;
+				if (!Obstructions.contains(CurrentLocation + CurrentDirection))
+				{
+					NumMatches++;
+					Obstructions.emplace(CurrentLocation + CurrentDirection);
+				}
 			}
-		// }
-
-		VisitedLocations.emplace_back(Grid.GetGuardLocation());
+			VisitedLocations.emplace(CurrentLocation);
+		}
 	}
-
-	for (auto visited_location : VisitedLocations)
-	{
-		std::cout << visited_location << '\n';
-	}
-
-	// return (int32_t)VisitedLocations.size();
-	return PossibleObstructions;
+	return NumMatches;
 }
 }
